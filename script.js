e9cf59ee145039140c30f3f2df69740a985e580f
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2024-01-14 21:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	let arr=s.split(" ");
+
+	return arr[0];
 }
 
 // Do not change the code below
-
+ 
 const s = prompt("Enter String:");
 alert(firstWord(s));
